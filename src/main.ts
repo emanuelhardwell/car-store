384035464ff9590fcea5cde00e3a60f3cb5be059
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-11 23:48:22 -0600

    feat: The config the server was updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(morgan('dev'));
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
+  console.log(`Server running on -----> ${await app.getUrl()}`);
 }
 bootstrap();
