c4c4b46b8ade5b2305bc14175d68b12b82075862
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-12 23:27:05 -0600

    feat: The ValidationPipe was configured in func main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remueve todo lo que no está incluído en los DTOs
+      forbidNonWhitelisted: true, // Retorna bad request si hay propiedades en el objeto no requeridas
+    }),
+  );
 
   app.enableCors();
   app.use(morgan('dev'));
