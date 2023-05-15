509d815bba931bfd05f683944bdc8266ea899364
emanuelhardwell <emanuelnoe16@gmail.com>
2023-05-14 19:43:24 -0600

    feat: The fillBrandsWithSeedData and fillCarsWithSeedData was created

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -58,4 +58,8 @@ export class BrandsService {
     this.findOne(id);
     this.brands = this.brands.filter((brand) => brand.id !== id);
   }
+
+  fillBrandsWithSeedData(brands: Brand[]) {
+    this.brands = brands;
+  }
 }
